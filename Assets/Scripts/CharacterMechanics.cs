4e57cf5003271acad80b85a1154fea4de219b9d9
Björn Johannsen <johannsen.bjoern@gmx.net>
2014-07-02 13:20:18 +0200

    Character Mechanics um die Upgrades für Horst erweitert (Hut, Stiefel, Lederweste)
    Variablen ein wenig verändert, für den Movement-Speed gibt es jetzt 2 Variablen: "m_BaseSpeed" und "m_Speed", Kommentare erklären warum

---
+++ Assets/Scripts/CharacterMechanics.cs
@@ -0,0 +1,130 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMechanics : MonoBehaviour
+{
+    public float m_BaseSpeed; //Wert zum abgleich für die Prozentuale erhöhung des Movementspeeds (Damit der shit nich stacked)
+    private float m_Speed; //Bewegungsgeschwindigkeit der Einheit
+    public float m_Damage; //Schaden den die Einheit verursacht (Sofern es nicht Horst ist, da sein verursachter Schaden von den Waffen festegelgt wird die er hält)
+    public float m_MaxHP; //Maximale HP der Einheit
+    private float m_HP; //Aktuelle HP der Einheit
+    public float m_RegHP; //HP5, s.h.: Menge an HP die alle 5 Sekunden generiert wird
+    public float m_Armor; //Rüstung die die Einheit besitzt, wird direkt von dem eingehenden Schaden abgezogen (Keine Rüstungsdurchdringung)
+    public bool m_IsAlive = true; //Gibt an ob die Einheit lebt oder nicht
+    private float timer = 0f; //Timer für HP5
+
+    //Upgrade Levels für Horst
+    public int m_HutLevel = 1;
+    public int m_StiefelLevel = 1;
+    public int m_WesteLevel = 1;
+
+    //Take Damage Funktion
+    public void TakeDamage(int damage)
+    {
+        float trueDamage = damage - m_Armor; //Armor wird direkt vom Schaden abgezogen, true Damage ist der Wert der im Endeffekt von den HP abgezogen wird
+        m_HP -= trueDamage;
+        if (m_HP <= 0f) //Abfrage ob HP auf 0 gesunken sind
+        {
+            m_HP = 0f;
+            m_IsAlive = false;
+        }
+        return;
+    }
+
+    //Funktion für die Levelsteigerung des Hutes
+    public void Hut()
+    {
+        switch (m_HutLevel)
+        {
+            case 0:
+                return;
+            case 1:
+                m_MaxHP += 5f;
+                return;
+            case 2:
+                m_MaxHP += 15f;
+                return;
+            case 3:
+                m_MaxHP += 30f;
+                return;
+            case 4:
+                m_MaxHP += 40f;
+                return;
+            case 5:
+                m_MaxHP += 50f;
+                return;
+        }
+    }
+
+    //Funktion für die Levelsteigerung der Lederweste
+    public void Weste()
+    {
+        switch (m_WesteLevel)
+        {
+            case 0:
+                return;
+            case 1:
+                m_Armor = 1f;
+                return;
+            case 2:
+                m_Armor = 2f;
+                return;
+            case 3:
+                m_Armor = 3f;
+                return;
+            case 4:
+                m_Armor = 4f;
+                return;
+            case 5:
+                m_Armor = 5f;
+                return;
+        }
+    }
+
+    //Funktion für die Levelsteigerung der Stiefel
+    public void Stiefel()
+    {
+        switch (m_StiefelLevel)
+        {
+            case 0:
+                return;
+            case 1:
+                m_Speed = m_BaseSpeed * 1.05f;
+                return;
+            case 2:
+                m_Speed = m_BaseSpeed * 1.10f;
+                return;
+            case 3:
+                m_Speed = m_BaseSpeed * 1.15f;
+                return;
+            case 4:
+                m_Speed = m_BaseSpeed * 1.20f;
+                return;
+            case 5:
+                m_Speed = m_BaseSpeed * 1.25f;
+                return;
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        //Setzt die aktuellen HP auf die Maximal-HP der Einheit hoch, wird nur beim Start aufgerufen
+        m_HP = m_MaxHP;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Regelt die HP-Regeneration
+        timer += Time.deltaTime;
+        if (timer >= 5)
+        {
+            m_HP += m_RegHP;
+            timer = 0f;
+            return;
+        }
+
+    }
+}
+
