43964d1364634baf1da0fd146d05d580f634542b
Jerry Haindl <goldenarrow818@hotmail.com>
2014-07-03 14:38:35 +0200

    GUI hinzugefügt + LMG Normal Texture zu Normal Texture Typ geändert

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,165 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour 
+{
+    private Gui m_gui;
+    private EGameState m_gameStates = EGameState.STARTMENU;
+	// Use this for initialization
+	void Start () 
+    {
+        GameStates = EGameState.STARTMENU;
+	}
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        m_gui = FindObjectOfType<Gui>();
+      //  m_Instance = this;
+    }
+    public EGameState GameStates 
+    { 
+        get 
+        {
+            return m_gameStates;
+        } 
+        set 
+        {
+            switch (m_gameStates)
+            {
+                case EGameState.STARTMENU:
+                    DeactivateStartMenu();
+                    break;
+                case EGameState.INGAME:
+                    DeactivateIngame();
+                    break;
+                case EGameState.PAUSE:
+                    DeactivatePause();
+                    break;
+                case EGameState.ITEMSHOP:
+                    DeactivateItemShop();
+                    break;
+                case EGameState.OPTIONS:
+                    DeactivateOptions();
+                    break;
+                case EGameState.ENDSCREEN:
+                    DeactivateEndScreen();
+                    break;
+                default:
+                    break;
+            }
+            switch (value)
+            {
+                case EGameState.STARTMENU:
+                    ActivateStartMenu();
+                    break;
+                case EGameState.INGAME:
+                    ActivateIngame();
+                    break;
+                case EGameState.PAUSE:
+                    ActivatePause();
+                    break;
+                case EGameState.ITEMSHOP:
+                    ActivateItemShop();
+                    break;
+                case EGameState.OPTIONS:
+                    ActivateOptions();
+                    break;
+                case EGameState.ENDSCREEN:
+                    ActivateEndScreen();
+                    break;
+                default:
+                    break;
+            }
+            m_gameStates = value;
+        } 
+    }
+
+    #region deactivate GameStates
+
+    private void DeactivateStartMenu()
+    {
+        m_gui.StartMenu.SetActive(false);
+      
+    }
+
+    private void DeactivateIngame()
+    {
+        m_gui.IngameUI.SetActive(false);   
+    }
+
+    private void DeactivatePause()
+    {
+        m_gui.PauseMenu.SetActive(false);
+    }
+
+    private void DeactivateItemShop()
+    {
+        m_gui.ItemShop.SetActive(false);
+    }
+
+    private void DeactivateOptions()
+    {
+       // m_gui.Options.SetActive(false);
+    }
+
+    private void DeactivateEndScreen()
+    {
+       // m_gui.EndScreen.SetActive(false);
+    }
+
+    #endregion
+
+    #region activate GameStates
+
+    private void ActivateStartMenu()
+    {
+        m_gui.StartMenu.SetActive(true);   
+    }
+
+    private void ActivateIngame()
+    {
+        m_gui.IngameUI.SetActive(true);
+        //Time.timeScale = 1f;
+    }
+
+    private void ActivatePause()
+    {
+        m_gui.PauseMenu.SetActive(true);
+        //Time.timeScale = 0f;
+    }
+
+    private void ActivateItemShop()
+    {
+        m_gui.ItemShop.SetActive(true);
+        //Time.timeScale = 0f;
+    }
+
+    private void ActivateOptions()
+    {
+       // m_gui.
+    }
+
+    private void ActivateEndScreen()
+    {
+        m_gui.EndScreen.SetActive(true);
+    }
+    //public static GameManager m_Instance;
+
+    #endregion
+
+}
+ public enum EGameState
+ {
+     STARTMENU,
+     INGAME,
+     PAUSE,
+     ITEMSHOP,
+     OPTIONS,
+     ENDSCREEN
+ }
+
---
+++ Assets/Scripts/Gui.cs
@@ -0,0 +1,139 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gui : MonoBehaviour
+{
+    public GameObject StartMenu;
+    public GameObject IngameUI;
+    public GameObject PauseMenu;
+    public GameObject ItemShop;
+    public GameObject EndScreen;
+
+    public UITexture EnemyHPBar;
+
+    public UITexture HPBar;
+    public UILabel Points;
+    public UILabel BuyPoints;
+    public UILabel WeaponNameIcon;
+    public UILabel Ammo;
+    public UISprite WeaponIcon;
+    public UISprite EnemyBossIcon;
+    public string[] weaponNames;
+
+    private UISprite m_CurrentWeaponSprite;
+    private int m_currentAmmo = 57;
+    private int m_maxAmmo = 200;
+    private int selectedWeaponIcon = 0;
+    private float m_currentHP = 1f;
+    private float m_enemyCurrentHP = 1f;
+    private float m_enemyMaxHP = 100f;
+    private float m_maxHP = 100f;
+    private float m_currentPoints = 10f;
+    private float m_currentBuyPoint = 10f;
+   // private bool m_testWeaponSwitch = true;
+    private bool isPaused = false;
+    private GameManager m_gameManager;
+
+    void Awake()
+    {
+        m_gameManager = FindObjectOfType<GameManager>();
+    }
+
+    void Start()
+    {
+        m_currentHP = m_maxHP;
+        m_enemyCurrentHP = m_enemyMaxHP;
+    }
+
+    void Update()
+    {
+        HPLoss();
+        EnemyHPLoss();
+        CurrentPoints();
+        Pause();
+    }
+
+    public void StartGame()
+    {
+        Application.LoadLevelAdditive(1);
+        m_gameManager.GameStates = EGameState.INGAME;
+        //IngameUI.SetActive(true);
+        //StartMenu.SetActive(false);
+      //  GameManager.m_Instance.GameStates = EGameState.INGAME;
+
+    }
+
+    public void Pause()
+    {
+        //PauseMenu.SetActive(false);
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+           
+            isPaused = !isPaused;
+            //Time.timeScale = isPaused ? 0:1;
+            if (isPaused == false)
+            {
+                m_gameManager.GameStates = EGameState.INGAME;
+                // IngameUI.SetActive(true);
+                // PauseMenu.SetActive(false);
+            }
+            else
+            { 
+                m_gameManager.GameStates = EGameState.PAUSE;
+            }
+        }
+
+    }
+
+    public void Continue()
+    {
+        isPaused = !isPaused;
+        m_gameManager.GameStates = EGameState.INGAME;
+        //Time.timeScale = isPaused ? 0 : 1;
+        // IngameUI.SetActive(true);
+        // PauseMenu.SetActive(false);
+    }
+
+    
+
+    public void Options()
+    {
+       
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void EnemyHPLoss()
+    {
+        EnemyHPBar.width = (int)(550 * m_enemyCurrentHP / m_enemyMaxHP);
+    }
+    public void HPLoss()
+    {
+        HPBar.width = (int)(135 * m_currentHP / m_maxHP);
+    }
+
+    public void CurrentPoints()
+    {
+        Points.text = m_currentPoints.ToString();
+        BuyPoints.text = m_currentBuyPoint.ToString();
+    }
+
+    public void HPDamage()
+    {
+        m_enemyCurrentHP = m_enemyCurrentHP - 1;
+    }
+
+    public void ChangeIcon()
+    {
+        selectedWeaponIcon = (selectedWeaponIcon + 1) % weaponNames.Length;
+        WeaponIcon.spriteName = weaponNames[selectedWeaponIcon];
+        WeaponNameIcon.text = weaponNames[selectedWeaponIcon];
+        Ammo.text = m_currentAmmo.ToString() + "/" + m_maxAmmo.ToString();        
+    }
+}
+
+
+
